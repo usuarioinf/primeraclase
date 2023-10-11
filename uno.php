73face39181968a069b5f533d30804c7b62a87c3
unknown <usuarioinf123@gmail.com>
2023-10-10 23:20:53 -0400

    se agrega comentarios

--- uno.php
+++ uno.php
@@ -8,6 +8,7 @@ use Luecano\NumeroALetras\NumeroALetras;
 echo "<br>";
 
 $formatter = new NumeroALetras();
+//este es una funcion para convertir a numeros
 echo $formatter->toWords(8716); echo " ";
 
 
